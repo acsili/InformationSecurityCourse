bb255dc2e165d102508a5b6d950ae6700cceefbb
IlyaOvchinnikov-0 <ilyaovchinnikov4@gmail.com>
2023-09-19 16:59:30 +0400

    Caesar Cipher

---
+++ InformationSecurity/CaesarСipher/CaesarCipherEncrypter.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace InformationSecurity.CaesarСipher;
+
+internal static class CaesarCipherEncrypter
+{
+    /// <summary>
+    /// Зашифровать текст.
+    /// </summary>
+    /// <param name="step"></param>
+    /// <param name="text"></param>
+    /// <returns></returns>
+    public static string EncryptString(int step, string text)
+    => string.Join("", text.ToLower().Select(x => (char)(x + step > 122 ? x + step - 'z' + 'a' - 1 : x + step)));
+
+    /// <summary>
+    /// Расшифровать текст.
+    /// </summary>
+    /// <param name="step"></param>
+    /// <param name="text"></param>
+    /// <returns></returns>
+    public static string DecryptString(int step, string text)
+    => string.Join("", text.ToLower().Select(x => (char)(x - step < 97 ? x - step + 'z' - 'a' + 1 : x - step)));
+
+    /// <summary>
+    /// Взломать шифр. 
+    /// </summary>
+    /// <param name="text"></param>
+    /// <returns></returns>
+    public static string HackCipher(string text)
+    {
+        var mostOftenLetter = text.ToLower().GroupBy(x => x).OrderBy(x => x.Count()).Last().Key; 
+        return DecryptString(Math.Abs(mostOftenLetter - 'e'), text);
+    }
+
+}
---
+++ InformationSecurity/Program.cs
@@ -0,0 +1,11 @@
+﻿
+using InformationSecurity.CaesarСipher;
+
+string text = "asdvbhesihqxzccbqegbwibewgkjeqbgwofiwnbegihbwieoiqgvnjmjknjansjqiwuhfewovdmxnjsdfz";
+
+var strEn = CaesarCipherEncrypter.EncryptString(6, text);
+var strDe = CaesarCipherEncrypter.DecryptString(6, strEn);
+
+Console.WriteLine(strEn);
+Console.WriteLine(strDe);
+Console.WriteLine(CaesarCipherEncrypter.HackCipher(text));
